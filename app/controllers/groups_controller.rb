b9f4329a4575a9a666b4eed7988f7d0d5c3b1fc3
niushuangmeng <niushuangmeng@gmail.com>
2017-07-05 16:55:41 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -23,9 +23,12 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.save
+    if @group.save
 
     redirect_to groups_path
+  else
+    render :new
+  end 
 
 end
 
